8e026259f98c611f63fce4c5582e961a7a54ddd1
Satyam Choubey <satyamchoubey789@gmail.com>
2025-08-01 21:05:15 +0530

    sus

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+// app/layout.tsx or app/layout.ts
+
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "@/components/Footer";
@@ -6,14 +8,17 @@ import HeaderWrapper from "@/components/HeadWrapper";
 import ReactQueryProvider from "@/utils/providers";
 import { CurrencyProvider } from "@/components/CurrencyProvider";
 import ClientCartWrapper from "@/components/ClientCartWrapper";
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import CustomCursor from "@/components/CustomCursor";
 
 export const metadata: Metadata = {
   title: "SAMAA | Luxury Candles & Stories",
   description: "Elegant luxury candles and inspiring stories by SAMAA.",
 };
+
+// GTM ID
+const GTM_ID = "GTM-5F66PPD9";
 
 export default function RootLayout({
   children,
@@ -22,7 +27,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');`,
+          }}
+        />
+      </head>
       <body className="min-h-screen flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <HeaderWrapper />
 
         <main className="flex-grow">
@@ -34,8 +61,8 @@ export default function RootLayout({
               <ClientCartWrapper />
             </CurrencyProvider>
           </ReactQueryProvider>
-           <SpeedInsights />
-            <Analytics />
+          <SpeedInsights />
+          <Analytics />
         </main>
 
         <Footer />
